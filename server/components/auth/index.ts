fe397819da1abfddcf632e51b7d102a1a4ba45c0
Mandar <mkmandar123@gmail.com>
2021-07-10 15:56:10 +0530

    Fix(statusCode): Status 401 for unauthorized requests

--- server/components/auth/index.ts
+++ server/components/auth/index.ts
@@ -1,6 +1,6 @@
 export default function (req: any, res: any, next: Function): any {
   if (!req.get('authorization') || (req.get('authorization').split(' ')[0] !== 'token')) {
-    return res.status(404).json({message: 'Unauthorized Request.'});
+    return res.status(401).json({message: 'Unauthorized Request.'});
   }
 
   return next();
